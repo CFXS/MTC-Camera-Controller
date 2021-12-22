e854a9cd87679c13bbb0bf02da7e0509c7419297
RVeips <rihardsveips@gmail.com>
2021-12-22 23:52:27 +0200

    lhg + ref
    Signed-off-by: RVeips <rihardsveips@gmail.com>

--- TimecodeCameraController/src/Core/CameraController.cpp
+++ TimecodeCameraController/src/Core/CameraController.cpp
@@ -1,17 +1,17 @@
 // ---------------------------------------------------------------------
 // CFXS TImecodeCameraController <https://github.com/CFXS/TimecodeCameraController>
 // Copyright (C) 2021 | CFXS
-//
+// 
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
-//
+// 
 // This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 // GNU General Public License for more details.
-//
+// 
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>
 // ---------------------------------------------------------------------
@@ -19,6 +19,7 @@
 #include "CameraController.hpp"
 #include "Profiles/CaptureCamera.hpp"
 #include <QMutex>
+#include <QTimer>
 
 namespace TCC {
 
@@ -29,11 +30,18 @@ namespace TCC {
     static constexpr float ROT_MUL_FAST   = 0.75f;
 
     static constexpr float DEFAULT_FOV = 75.0f;
+
+    static constexpr auto DMX_SEND_RATE = 44;
     ///////////////////////////////////////////////////////////////
 
     CameraController::CameraController(QObject* parent) : QObject(parent) {
         printf("Create CameraController\n");
         Reset();
+
+        auto outputTimer = new QTimer(this);
+        connect(outputTimer, &QTimer::timeout, [=]() {
+        });
+        outputTimer->start(1000 / DMX_SEND_RATE);
     }
 
     void CameraController::SetNetworkInterface(const QString& newAddress) {
