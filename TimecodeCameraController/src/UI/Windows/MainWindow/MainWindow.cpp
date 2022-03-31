73884f75c0c6e4f08ff03bee7ef1573f0edcb85e
RVeips <rihardsveips@gmail.com>
2022-03-31 23:12:07 +0300

    test stuff
    Signed-off-by: RVeips <rihardsveips@gmail.com>

--- TimecodeCameraController/src/UI/Windows/MainWindow/MainWindow.cpp
+++ TimecodeCameraController/src/UI/Windows/MainWindow/MainWindow.cpp
@@ -1,17 +1,17 @@
 // ---------------------------------------------------------------------
 // CFXS MTC Camera Controller <https://github.com/CFXS/MTC-Camera-Controller>
 // Copyright (C) 2021 | CFXS
-// 
+//
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
-// 
+//
 // This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 // GNU General Public License for more details.
-// 
+//
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>
 // ---------------------------------------------------------------------
@@ -187,6 +187,7 @@ namespace TCC::UI {
 
     void MainWindow::SetMTC_Port(const QString& name) {
         int cbIndex = 1;
+
         for (auto& entry : m_MIDI->GetDevices()) {
             if (entry.name == name) {
                 m_MIDI->SetCurrentDevice(entry.index);
@@ -339,9 +340,9 @@ namespace TCC::UI {
     void MainWindow::UpdateStatusBar() {
         if (IsProjectOpen()) {
             ui->statusbar->showMessage("Project Path: " + m_ProjectPath + " | " +
-                                       QString(IsGamepadActive() ? "Gamepad Active" : "Gamepad Not Detected"));
+                                       QString(IsGamepadActive() ? "Gamepad active" : "Gamepad not detected"));
         } else {
-            ui->statusbar->showMessage(IsGamepadActive() ? "Gamepad input active" : "Gamepad not detected");
+            ui->statusbar->showMessage(IsGamepadActive() ? "Gamepad active" : "Gamepad not detected");
         }
     }
 
